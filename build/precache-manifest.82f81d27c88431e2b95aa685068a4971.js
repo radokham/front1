35d6ef2b355d71ed92d1c63f5e23e18658a34133
radokham <radokham6@gmail.com>
2019-07-12 15:15:10 +0300

    message

---
+++ build/precache-manifest.82f81d27c88431e2b95aa685068a4971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e100fcbd025bb36f2f5",
+    "url": "/static/css/main.b7e6e4fd.chunk.css"
+  },
+  {
+    "revision": "5e100fcbd025bb36f2f5",
+    "url": "/static/js/main.4c269b9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "827f0a09f9c01ed50b7a",
+    "url": "/static/css/2.c7cf80df.chunk.css"
+  },
+  {
+    "revision": "827f0a09f9c01ed50b7a",
+    "url": "/static/js/2.15e7593f.chunk.js"
+  },
+  {
+    "revision": "a16ffd25393299985bd5371b53322948",
+    "url": "/index.html"
+  }
+];
